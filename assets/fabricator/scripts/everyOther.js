0fe97020324962b125bb97e421494fdc65002d47
katarzynaMW <katarzyna.juszczyk@makingwaves.com>
2017-06-20 17:35:15 +0200

    Wrong assets

--- assets/fabricator/scripts/everyOther.js
+++
@@ -1,8 +0,0 @@
-
-var Handlebars = require('handlebars');
-Handlebars.registerHelper("everyOther", function (index, amount, scope) {
-    if ( ++index % amount)
-        return scope.inverse(this);
-    else
-        return scope.fn(this);
-});
